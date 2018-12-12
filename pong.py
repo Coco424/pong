262d4c4a2bb279da5323873718563c8d8c1dae02
Corinne COUPIER <coupier.corinne@gmail.com>
2018-12-12 10:56:05 +0100

    bouger la raquette et tourner la balle en rond

--- pong.py
+++ pong.py
@@ -27,3 +27,68 @@ while 1:
         break
     sense.set_pixel(x, y, balle)
     time.sleep(0.2)    
+from sense_hat import SenseHat
+import time
+sense = SenseHat()
+sense.clear();
+
+raquette=(0,0,255)
+balle=(255,255,255)
+noir=(0,0,0)
+x = 3 
+y = 7
+depx = 1
+depy = 1
+
+#affichage raquette bleu au bord (x, y, r, g, b)
+sense.set_pixel(0, 0, raquette)
+sense.set_pixel(0, 1, raquette)
+sense.set_pixel(0, 2, raquette)
+
+#affichage balle blanche au centre (x, y, r, g, b)
+sense.set_pixel(x, y, balle)
+
+while 1:
+    sense.set_pixel(x, y, noir)
+    x -= depx
+    if x == 0 or x == 7:
+        depx *= -1
+    y -= depy
+    if y == 0 or y == 7:
+        depy *= -1
+    sense.set_pixel(x, y, balle)
+    time.sleep(0.2)    
+
+from sense_hat import SenseHat
+import time
+sense = SenseHat()
+sense.clear();
+
+raquette=(0,0,255)
+balle=(255,255,255)
+noir=(0,0,0)
+x = 3 
+y = 7
+z = 4
+depx = 1
+depy = 1
+
+#affichage raquette bleu au bord (x, y, r, g, b)
+sense.set_pixel(0, z+0, raquette)
+sense.set_pixel(0, z+1, raquette)
+sense.set_pixel(0, z+2, raquette)
+
+#affichage balle blanche au centre (x, y, r, g, b)
+sense.set_pixel(x, y, balle)
+
+while 1:
+    sense.set_pixel(x, y, noir)
+    x -= depx
+    if x == 0 or x == 7:
+        depx *= -1
+    y -= depy
+    if y == 0 or y == 7:
+        depy *= -1
+    sense.set_pixel(x, y, balle)
+    time.sleep(0.2)    
+
